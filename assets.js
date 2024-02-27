6e6f7c00154691faaa8d2606620f8820b784d973
ulfapivis <ulf@apivis.com>
2024-02-28 00:48:37 +0100

    Update assets.js

--- assets.js
+++ assets.js
@@ -82,7 +82,7 @@ header h2 {
 }
 .chatbot .chatbox {
   overflow-y: auto;
-  height: 510px;
+  height: 400px;
   padding: 30px 30px 120px;
 }
 .chatbot :where(.chatbox, textarea)::-webkit-scrollbar {
